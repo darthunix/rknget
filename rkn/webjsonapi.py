f16deb34c9cb6169ee14a65ea45db639b52bb603
user <user@localhost>
2019-02-26 18:56:24 +0300

    redis

--- rkn/webjsonapi.py
+++ rkn/webjsonapi.py
@@ -2,26 +2,57 @@
 import json
 import sys
 
-from dbconn import connstr
+import dbconn
+import redis
 
 
-def main():
-    jsin = json.loads(sys.stdin.read())
+def _getParamsDict():
+    return json.loads(sys.stdin.read())
 
-    modval = jsin.pop('module', None)
-    if modval.split('.')[0] != 'api':
-        print('Content-Type: text/plain\r\n\r\nNot an API')
-        return 1
-    metval = jsin.pop('method', None)
-    module = __import__(modval, fromlist=[metval])
-    jsin['connstr'] = connstr
 
-    # Shoot your leg through!!!
-    data = getattr(module, metval)(**jsin)
-
+def printData(data):
     print("Content-type:application/json\r\n\r\n")
     print(json.dumps(data, default=str))
 
+
+def main():
+    params = _getParamsDict()
+    fields = params.copy()
+    modval = fields.pop('module', None)
+    if modval.split('.')[0] != 'api':
+        print('Content-Type: text/plain\r\n\r\nNot an API')
+        return 1
+    metval = fields.pop('method', None)
+
+    # Redis part
+    rdb = None
+    rdbvaluekey = None
+    if modval in dbconn.rdb.cache:
+        if dbconn.rdb.conn:
+            rdb = redis.Redis(**dbconn.rdb.conn)
+            try:
+                rdbvaluekey = ''.join(sorted(map(str, list(params.keys()) + list(params.values()))))
+                data = rdb.get(rdbvaluekey)
+                if data:
+                    printData(data)
+                    return 0
+            except redis.TimeoutError:
+                print('Redis timeout', file=sys.stderr)
+                rdb = None
+
+    # Shoot your leg through!!!
+    module = __import__(modval, fromlist=[metval])
+    fields['connstr'] = dbconn.connstr
+    data = getattr(module, metval)(**fields)
+
+    # Redis part
+    if rdb:
+        try:
+            rdb.set(rdbvaluekey, str(data), ex=dbconn.rdb.ex)
+        except redis.TimeoutError:
+            print('Redis timeout', file=sys.stderr)
+
+    printData(data)
     return 0
 
 
