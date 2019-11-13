da19b6f8df4fce9eb20128e66163d7225b53ad14
user <user@localhost>
2019-11-13 15:26:43 +0300

    MAJOR BUG: absent entries were not marked as in_dump=False

--- rkn/db/dataprocessing.py
+++ rkn/db/dataprocessing.py
@@ -165,7 +165,7 @@ def updateContentPresence(dump_id, disabledIDList=[]):
     :return: True
     """
     cursor.execute(
-        '''UPDATE content SET in_dump = True
+        '''UPDATE content SET in_dump = False
         WHERE outer_id = ANY(%s)''',
         (disabledIDList,)
     )
